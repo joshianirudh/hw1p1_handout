a3a771c29ff2cc07d000b2ca1ab266f4d58e2d85
joshianirudh <joshianirudh26@gmail.com>
2025-01-21 21:20:14 -0500

    linear implemented

--- mytorch/nn/linear.py
+++ mytorch/nn/linear.py
@@ -8,8 +8,8 @@ class Linear:
         Read the writeup (Hint: Linear Layer Section Table) to identify the right shapes for `W` and `b`.
         """
         self.debug = debug
-        self.W = np.zeros(out_features, in_features)  # TODO
-        self.b =   # TODO
+        self.W = np.zeros((out_features, in_features))  # TODO
+        self.b = np.zeros((out_features, 1))# TODO
 
     def forward(self, A):
         """
@@ -18,14 +18,15 @@ class Linear:
 
         Read the writeup (Hint: Linear Layer Section) for implementation details for `Z`
         """
-        self.A = None  # TODO
-        self.N = None  # TODO - store the batch size parameter of the input A
+        self.A = A  # TODO
+        self.N = A.shape[0]  # TODO - store the batch size parameter of the input A
 
         # Think how can `self.ones` help in the calculations and uncomment below code snippet.
         # self.ones = np.ones((self.N, 1))
 
-        Z = None  # TODO
-        raise NotImplemented  # TODO - What should be the return value?
+        Z = np.dot(self.A, self.W.T) +  self.b.T  # TODO
+        print("Z is:", Z)
+        return Z  # TODO - What should be the return value?
 
     def backward(self, dLdZ):
         """
@@ -34,11 +35,11 @@ class Linear:
 
         Read the writeup (Hint: Linear Layer Section) for implementation details below variables.
         """
-        dLdA = None  # TODO
-        self.dLdW = None  # TODO
-        self.dLdb = None  # TODO
+        dLdA = np.dot(dLdZ, self.W)  # TODO
+        self.dLdW = np.dot(dLdZ.T, self.A)  # TODO
+        self.dLdb = np.sum(dLdZ, axis = 0, keepdims=True).T  # TODO
 
         if self.debug:
             self.dLdA = dLdA
-
-        raise NotImplemented  # TODO - What should be the return value?
+        print(dLdA)
+        return dLdA  # TODO - What should be the return value?
